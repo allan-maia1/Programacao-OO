16a14f28890991027dd919b3c88e40d14450db5b
Allan Maia <celallan@outlook.com>
2026-09-09 12:23:56 -0300

    Add files via upload
    Tarefas feitas no estudo de Orientação a Obejtos

---
+++ classes/oo/Contador.java
@@ -0,0 +1,9 @@
+package oo;
+
+public class Contador {
+	public static int count =0;
+	
+	public Contador(){
+		count++;
+	}
+}
---
+++ classes/oo/Pessoa.java
@@ -0,0 +1,16 @@
+package oo;
+
+public class Pessoa {
+	String nome, profissao;
+	int idade;
+	
+	public Pessoa(String nome, int idade, String profissao) {
+		this.nome=nome;
+		this.idade=idade;
+		this.profissao=profissao;
+	}
+	
+	void exibirDados() {
+		System.out.println("Nome: "+ nome + "\nIdade: "+idade + "\nProfissão: "+ profissao);
+	}
+}
